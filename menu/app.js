5cf15cb05114894098f482cad4bfe9fbd1e61ca0
LuisBueno01 <hola162001@gmail.com>
2026-05-12 01:47:20 -0600

    Update app.js

--- menu/app.js
+++ menu/app.js
@@ -794,7 +794,7 @@ async function loadDynamicData() {
 }
 
 async function doLogout() {
-  try { await signOut(auth); window.location.href = '../Paguinasprincipal/index.html'; }
+  try { await signOut(auth); window.location.href = 'FIEEPruebabeta/index.html'; }
   catch (e) { console.error(e); }
 }
 window.doLogout = doLogout;
